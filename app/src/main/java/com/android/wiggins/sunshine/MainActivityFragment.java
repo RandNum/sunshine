492aa99565ac5809b82b179dd5205ca5483e3165
wiggins <wiggins2000@gmail.com>
2015-06-09 18:28:05 +0200

    first init commit for Sunshine project

--- app/src/main/java/com/android/wiggins/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/android/wiggins/sunshine/MainActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -18,6 +22,23 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayList<String> forecastEntries = new ArrayList<>();
+        forecastEntries.add("Today - Sunny - 88/63");
+        forecastEntries.add("Tomorrow - Foggy - 70/46");
+        forecastEntries.add("Weds - Cloudy - 88/63");
+        forecastEntries.add("Thurs - Rainy - 88/63");
+        forecastEntries.add("Friday - Foggy - 88/63");
+        forecastEntries.add("Sat - Sunny - 88/63");
+
+        //Use above created array data to populate ArrayAdaptar object
+        ArrayAdapter forecastAdaptar = new ArrayAdapter(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, forecastEntries);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list_item_forecast);
+        listView.setAdapter(forecastAdaptar);
+
+        return rootView;
     }
 }
